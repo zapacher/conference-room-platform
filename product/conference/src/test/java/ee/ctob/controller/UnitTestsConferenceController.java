23a79b68eba07908be0cbca6e2a575f922c697d5
Toomas Park <toomas.park@bigwintek.com>
2024-12-02 18:23:57 +0200

    WIP tests

--- product/conference/src/test/java/ee/ctob/controller/UnitTestsConferenceController.java
+++ product/conference/src/test/java/ee/ctob/controller/UnitTestsConferenceController.java
@@ -4,22 +4,19 @@ import ee.ctob.access.ConferenceDAO;
 import ee.ctob.api.Request;
 import ee.ctob.api.Response;
 import ee.ctob.api.controller.ConferenceController;
-import ee.ctob.data.Conference;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import testutils.TestContainer;
 
 import java.util.UUID;
 
-
-import static org.junit.jupiter.api.Assertions.assertAll;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 import static testutils.ObjectCreators.createRegitrationRequest;
 
 @Testcontainers
@@ -33,16 +30,56 @@ public class UnitTestsConferenceController extends TestContainer {
     ConferenceController controller;
 
     Request request;
-
     Response response;
+    UUID conferenceUUID;
+    UUID participantValidationUUID;
 
     @Test
     void participantRegistration() {
+        conferenceUUID = UUID.randomUUID();
+        request = createRegitrationRequest(conferenceUUID);
+        response = controller.registration(request);
+        mockRegistration(UUID.randomUUID());
+        assertAll("Registration Success",
+                ()-> assertNotNull(response, "Response"),
+                ()-> assertNotNull(response.getValidationUUID(), "validationUUID"),
+                ()-> assertNull(response.getReason(), "reason")
+        );
+        participantValidationUUID = response.getValidationUUID();
+    }
+
+    @Test
+    void participantRegistrationFail() {
         request = createRegitrationRequest(UUID.randomUUID());
         response = controller.registration(request);
-        assertAll("Registration success",
-                ()-> assertNotNull(response, "Response")
+
+        assertAll("Registration Fail",
+                ()-> assertNotNull(response, "Response"),
+                ()-> assertNull(response.getValidationUUID(), "validationUUID"),
+                ()-> assertEquals("Registration isn't available for this conference", response.getReason(), "response")
         );
-        System.out.println(response);
+    }
+    @Test
+    void registrationCancel() {
+
+    }
+
+    @Test
+    void registrationCancelFail() {
+
+    }
+
+    @Test
+    void availableConferences() {
+
+    }
+
+    @Test
+    void feedback() {
+
+    }
+
+    private void mockRegistration(UUID participantUUID) {
+        when(conferenceDAO.registerParticipant(participantUUID, conferenceUUID)).thenReturn(1);
     }
 }
